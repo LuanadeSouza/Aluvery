ec5db8db021862779446e4b49148d3871d13d27d
Luana <luana@wezo.com.br>
2024-12-12 18:29:24 -0300

    Primeiro commit

---
+++ app/src/main/java/com/luanadev/aluvery/sampledata/SampleData.kt
@@ -0,0 +1,22 @@
+package com.luanadev.aluvery.sampledata
+
+
+import java.math.BigDecimal
+
+val sampleProducts = listOf(
+    Product(
+        name = "Hamburguer",
+        price = BigDecimal("12.99"),
+        image = R.drawable.burger
+    ),
+    Product(
+        name = "Pizza",
+        price = BigDecimal("19.99"),
+        image = R.drawable.pizza
+    ),
+    Product(
+        name = "Batata frita",
+        price = BigDecimal("7.99"),
+        image = R.drawable.fries
+    )
+)
